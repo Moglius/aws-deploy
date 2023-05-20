19790c8a97918f4aa69c1b2bf5726eff4b87f00e
Matias Moglia <matiasmoglia@gmail.com>
2023-05-20 15:29:27 -0300

    adding more vars

--- main.tf
+++ main.tf
@@ -12,6 +12,27 @@ variable "instance_tag_name" {
     sensitive = false
 }
 
+variable "instance_type" {
+    type = string
+    description = "Instance type"
+    default = "t1.micro"
+    sensitive = false
+}
+
+variable "ami_id" {
+    type = string
+    description = "AMI id"
+    default = "ami-0889a44b331db0194"
+    sensitive = false
+}
+
+variable "availability_zone" {
+    type = string
+    description = "AZ"
+    default = "us-east-1a"
+    sensitive = false
+}
+
 terraform {
   required_providers {
     aws = {
@@ -27,8 +48,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "vm-web" {
-  ami           = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  availability_zone = var.availability_zone
 
   tags = {
     Name = var.instance_tag_name
